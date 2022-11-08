c8995a6c73c755d8ceeb7b26e8db324525460a08
Breno Marcomini <brenomarcomini@hotmail.com>
2022-11-07 22:51:01 -0300

    Componentização ancora

--- pages/faq.js
+++ pages/faq.js
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from "../src/components/Link";
 
 export default function FAQPage(){
     return (
         <div>
             <h1>Hello World!!!! Segundo Código Next.JS!!!!</h1>
-            <Link href="/">
-                Index
+            <Link href='/' class="Teste 2">
+                Ir para o Home
             </Link>
 
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from "../src/components/Link";
 
 export default function HomePage(){
     return (
         <div>
             <h1>Hello World!!!! Primeiro Código Next.JS!!!!</h1>
-            <Link href="/faq">
-                FAQ
+            <Link href='/faq' class="Teste">
+                Ir para o FAQ
             </Link>
 
         </div>
