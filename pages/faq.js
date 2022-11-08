92226e63e4c1171405c11d45b2f45394bb6e0849
Breno Marcomini <brenomarcomini@hotmail.com>
2022-11-07 22:22:40 -0300

    primeiras rotas

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage(){
+    return (
+        <div>
+            <h1>Hello World!!!! Segundo Código Next.JS!!!!</h1>
+            <a href="/">Index</a>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function HomePage(){
     return (
         <div>
             <h1>Hello World!!!! Primeiro Código Next.JS!!!!</h1>
+            <a href="/faq">FAQ</a>
         </div>
     )
 }
